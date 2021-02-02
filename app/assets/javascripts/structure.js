26e67dbff8d48fb960576f204838017290ec4c1b
Sam Kortekaas <samkortekaas@protonmail.com>
2021-02-02 17:09:28 +0100

    Added backbone + handlebars

---
+++ app/assets/javascripts/structure.js
@@ -0,0 +1,25 @@
+//= require_self
+//= require_tree ./models
+//= require_tree ./views
+//= require_tree ./routers
+//= require_tree ./templates
+
+window.AppClasses = {
+	Models: {},
+	Collections: {},
+	Views: {},
+	Routers: {}
+};
+
+window.App = {
+	models: {},
+	collections: {},
+	views: {},
+	routers: {},
+	templates: {},
+	data: {}
+};
+
+function test() {
+    alert('hoi');
+};
