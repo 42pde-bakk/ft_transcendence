9ca14a88458ccec7cb7d7167e328f4810d8bb512
Sam Kortekaas <samkortekaas@protonmail.com>
2021-02-02 20:04:00 +0100

    Added tailwindCSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import  "../layouts/application.css";
 import "channels"
 
 Rails.start()
