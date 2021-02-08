14fd574fda141d0a1fdc50fe2c963046c75e09c4
Sam Kortekaas <samkortekaas@protonmail.com>
2021-02-08 19:16:26 +0100

    Added def img_path for new user

--- db/migrate/20210203021846_create_users.rb
+++ db/migrate/20210203021846_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :img_path
+      t.string :img_path, default: "https://img2.cgtrader.com/items/2043799/e1982ff5ee/star-wars-rogue-one-solo-stormtrooper-helmet-3d-model-stl.jpg"
       t.string :token
       t.integer :guild_id
       t.boolean :tfa
