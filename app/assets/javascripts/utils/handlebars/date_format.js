1e9abd6b9ce331227b18e2077975be33e0bdbbeb
Sam Kortekaas <samkortekaas@protonmail.com>
2021-02-24 17:36:13 +0100

    Now showing wartime correctly

--- app/assets/javascripts/utils/handlebars/date_format.js
+++ app/assets/javascripts/utils/handlebars/date_format.js
@@ -1,4 +1,5 @@
 Handlebars.registerHelper('date_format', function (date, options) {
     const formatToUse = (arguments[1] && arguments[1].hash && arguments[1].hash.format) || "DD/MM/YYYY"
-    return moment(date).format(formatToUse);
+    console.log(date)
+    return moment.utc(date).format(formatToUse);
 });
