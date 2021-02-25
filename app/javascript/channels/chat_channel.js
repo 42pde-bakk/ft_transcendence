bba89cd656cdddc68fd742415238a82ffe5759ad
Peer de Bakker <pde-bakk@student.codam.nl>
2021-02-25 23:22:29 +0100

    I thought data['title'] was a string, but I guess not

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -21,7 +21,7 @@ function manageChatChannels() {
 					let chat_target_id = chat_div.getAttribute("data-chat-target-id");
 					if (data !== null && chat_target_id !== "0" && last_message !== data) {
 						last_message = data;
-						if (data["title"] === chat_target_id) {
+						if (data["title"] === parseInt(chat_target_id)) {
 							$('chat_log').append("<br>" + data["body"]);
 						}
 						console.log(`I have received "${data["body"]}" from ChatChannel_${data["title"]}`);
