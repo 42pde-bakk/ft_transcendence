a32e1acf979f61e7cc1720efd18fbaa0e4644fa0
Sam Kortekaas <samkortekaas@protonmail.com>
2021-02-25 13:36:33 +0100

    idk

--- app/assets/javascripts/utils/handlebars/date_format.js
+++ app/assets/javascripts/utils/handlebars/date_format.js
@@ -1,4 +1,4 @@
 Handlebars.registerHelper('date_format', function (date, options) {
     const formatToUse = (arguments[1] && arguments[1].hash && arguments[1].hash.format) || "DD/MM/YYYY"
-    return moment.utc(date).format(formatToUse);
+    return moment(date).format(formatToUse);
 });
