5e0421f23e9d70339ddb63e99d3b195dd57fca33
Peer de Bakker <pde-bakk@student.codam.nl>
2021-02-28 18:14:52 +0100

    added chatroom

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,4 @@
 class Chatroom < ApplicationRecord
 	belongs_to :owner, class_name: "User", required: true
-	has_many :
+	has_many :messages, dependent: :destroy
 end
--- db/migrate/20210228162655_create_chatrooms.rb
+++ db/migrate/20210228162655_create_chatrooms.rb
@@ -1,7 +1,10 @@
 class CreateChatrooms < ActiveRecord::Migration[6.1]
   def change
     create_table :chatrooms do |t|
-
+      t.string :name
+      t.string :privacy
+      t.string :password
+      t.references :owner, index: true, foreign_key: { to_table: :users}
       t.timestamps
     end
   end
